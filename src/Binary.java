485cbba513a2047c1bbe24fa7c4335eecec8a1d0
Carlos Eduardo <cb473355@gmail.com>
2025-06-20 12:37:38 -0300

    segubdo

--- src/Binary.java
+++ src/Binary.java
@@ -5,15 +5,16 @@ public class Binary {
     public static void main(String[] args) {
         var value1 = 6;
         var binary1 = Integer.toBinaryString(value1);
-        System.out.printf("Primeiro número da operação: %s (REPRESENTAÇÃO BINARIA %s)\n ", value1, binary1);
+        System.out.printf("Primeiro número da operação: %s (REPRESENTAÇÃO BINARIA %s)\n", value1, binary1);
+        
         var value2 = 5;
-        var binary2 = Integer.toBinaryString(value1);
-        System.out.printf("Segundo número da operação: %s (REPRESENTAÇÃO BINARIA %s)\n ", value2, binary2);
+        var binary2 = Integer.toBinaryString(value2); // Corrigido: estava usando value1 em vez de value2
+        System.out.printf("Segundo número da operação: %s (REPRESENTAÇÃO BINARIA %s)\n", value2, binary2);
+        
         var result = value1 + value2;
         var binaryResult = Integer.toBinaryString(result);
-        System.out.printf("%s + %s = %s (REPRSENTAÇÃO BINÁRIA %s)\n", value1, value2, result + binaryResult);
+        System.out.printf("%s + %s = %s (REPRSENTAÇÃO BINÁRIA %s)\n", value1, value2, result, binaryResult);
     }
-    
 }
 
 
