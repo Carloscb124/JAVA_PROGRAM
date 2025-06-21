6cad881b1a92317d3f412954b08dce9f3eee22b6
Carlos Eduardo <cb473355@gmail.com>
2025-06-21 12:51:39 -0300

    Me perdi nos commit: adicionando exercícios Java

--- src/App.java
+++ src/App.java
@@ -2,15 +2,15 @@ import java.util.Scanner;
 
 public class App{
 	public static void main(String[] args){
-        try (Scanner scanner = new Scanner(System.in)) {
+        Scanner scanner = new Scanner(System.in);
            
             System.out.print("Fale seu nome: ");
             String nome = scanner.next();
            
-            System.out.println("Qual é a sua idade? "+ nome);
+            System.out.print("Qual é a sua idade? "+ nome);
             int age = scanner.nextInt();
            
             System.out.println("Olá "+ nome + "! Seja bem vindo ao java!!, sua idade é " + age + " anos.");
         }
     }
-}
+
